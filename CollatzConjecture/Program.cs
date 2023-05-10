7c49330ed9b1bcbd1929302d35056961a152071c
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-05-10 16:13:59 +0100

    Add files via upload

---
+++ CollatzConjecture/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+string input;
+int intInput;
+bool isValid(int n)
+{
+    if (n > 0)
+    {
+        return true;
+    } else
+    {
+        return false;
+    }
+}
+
+bool isEven(int num)
+{
+    if ((num % 2) == 0) { return true; } else { return false;}
+}
+
+void calculate(int n)
+{
+    if (n == 1)
+    {
+        Console.WriteLine("Completed!");
+    }
+
+    else if(isEven(n))
+    {
+        n = n / 2;
+
+        Console.WriteLine($"{n} ");
+
+        calculate(n);
+
+    } else
+    {
+        n = (n * 3) + 1;
+
+        Console.WriteLine($"{n} ");
+
+        calculate(n);
+    }
+}
+
+Console.Write("Please enter an integer (greater than zero): ");
+
+input = Console.ReadLine();
+
+intInput = (int)Convert.ToInt64(input);
+
+if (isValid(intInput)) {
+
+    calculate(intInput);
+} else
+{
+    Console.WriteLine("Input not valid!");
+
+}
